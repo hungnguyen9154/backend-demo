c127d5f7b5d3deeebb126cefd8f0531fe8d31cd5
Nguyen Hung <hungnguyen9154@gmail.com>
2025-12-19 10:11:07 +0700

    backend ok

--- server.js
+++ server.js
@@ -6,4 +6,11 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://hungnguyen9154_db_user:Hungnguyen9154@cluster0.ksj6m4e.mongodb.net/?appName=Cluster0")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
  
