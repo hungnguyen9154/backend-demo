cdf6ac46b9326feb7d9782cf53bbc0f88608a597
Nguyen Hung <hungnguyen9154@gmail.com>
2025-12-19 12:43:42 +0700

    hola

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
  res.send("Backend Running!!! CI/CD OK");
 });
-
 mongoose.connect(process.env.MONGODB_URI)
  .then(() => console.log("MongoDB Connected"))
  .catch(err => console.log(err));
-
 app.listen(PORT, () => console.log("Server running on port", PORT));
