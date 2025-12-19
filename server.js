488f3c2e4e7d6fafd2e072c1db6027e6051e520b
Nguyen Hung <hungnguyen9154@gmail.com>
2025-12-19 11:52:44 +0700

    fix server port and mongodb env

--- server.js
+++ server.js
@@ -5,16 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Backend Running OK 🎉");
+ res.send("Backend Running!!! CI/CD OK");
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
-});
+mongoose.connect(process.env.MONGODB_URI)
+ .then(() => console.log("MongoDB Connected"))
+ .catch(err => console.log(err));
 
-// connect mongo nhưng KHÔNG được làm app stop
-mongoose.connect(process.env.MONGODB_URI, {
-  serverSelectionTimeoutMS: 5000
-})
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log("MongoDB Failed:", err.message));
+app.listen(PORT, () => console.log("Server running on port", PORT));
